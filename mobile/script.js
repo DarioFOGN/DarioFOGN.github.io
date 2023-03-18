4457a50ddfa59d0ef4c8d99dfa2341f726387490
Dario Pillinini <77980083+DarioFOGN@users.noreply.github.com>
2023-03-18 14:22:02 +0100

    update

--- mobile/script.js
+++ mobile/script.js
@@ -1,9 +1,11 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-  document.getElementById("mySidenav").style.width = "600px";
+  document.getElementById("mySidenav").style.width = "55%";
+  document.getElementById("stripeVoidA").style.width = "45%"
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("stripeVoidA").style.width = "0";
 }
