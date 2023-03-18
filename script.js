0ecfd933ce073912cce95cd581884866997b3c3f
Dario Pillinini <77980083+DarioFOGN@users.noreply.github.com>
2023-03-18 10:13:51 +0100

    update

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-if (navigator.userAgentData.mobile || screen.width <= 850) {
+if (navigator.userAgentData.mobile || screen.width <= 850 || WURFL.is_mobile == true) {
   window.location = "./mobile/index.html";
 }
